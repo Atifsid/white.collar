6a690d5769b95172ff4a164d76f8336795ae45ab
atifsiddiqui-betaarrays <atif.siddiqui@betaarrays.com>
2024-07-31 16:35:57 +0530

    initialize

---
+++ components/main/HeroSection.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import { ImagesSlider } from "../ui/images-slider";
+import { motion } from "framer-motion";
+import { FlipWords } from "../ui/flip-words";
+
+function HeroSection() {
+    const words = ["Election Campaigns", "Advertising", "Ad Films", "Portfolios", "Photo Shoots"];
+    const images = [
+        "https://images.unsplash.com/photo-1485433592409-9018e83a1f0d?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1482189349482-3defd547e0e9?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    ];
+    return (
+        <ImagesSlider className="h-[40rem]" images={images}>
+            <motion.div
+                initial={{
+                    opacity: 0,
+                    y: -80,
+                }}
+                animate={{
+                    opacity: 1,
+                    y: 0,
+                }}
+                transition={{
+                    duration: 0.6,
+                }}
+                className="z-50 flex flex-col justify-center items-center"
+            >
+                <motion.p className="font-bold text-4xl md:text-5xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+                    The
+                    <span className="text-orange-500 dark:text-orange-900">{` Random `}</span>
+                    Productions
+                </motion.p>
+
+                <div className="flex justify-center items-center">
+                    <div className="text-xl mx-auto font-normal text-neutral-200 dark:text-neutral-600">
+                        We can help you with
+                        <FlipWords words={words} className="text-orange-500 dark:text-orange-900" />
+                    </div>
+                </div>
+            </motion.div>
+        </ImagesSlider>
+    );
+}
+
+export default HeroSection
---
+++ tailwind.config.ts
@@ -0,0 +1,79 @@
+import type { Config } from "tailwindcss";
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: "#00040f", // Your primary background color
+        secondary: "#00f6ff", // Your secondary accent color
+        dimWhite: "rgba(255, 255, 255, 0.7)", // A dimmed white color
+        dimBlue: "rgba(9, 151, 124, 0.1)", // A dimmed blue color
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], // Use the Poppins font family
+      },
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+      backgroundImage: {
+        "nav-focus":
+          "linear-gradient(270deg, rgba(255, 255, 255, 0.06) 0%, rgba(255, 255, 255, 0.00) 100%)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [addVariablesForColors],
+} satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+export default config;
