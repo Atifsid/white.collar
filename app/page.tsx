3d9b762bcaffebfc4dce1194af5c51237613b8a6
atifsiddiqui-betaarrays <atif.siddiqui@betaarrays.com>
2024-07-31 17:47:55 +0530

    chore: add grid section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import GridSection from "@/components/main/GridSection";
 import HeroSection from "@/components/main/HeroSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <GridSection />
     </main>
   );
 }
---
+++ components/main/GridSection.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { LayoutGrid } from '../ui/layout-grid';
+
+function GridSection() {
+  return (
+    <div className="h-screen py-14 w-full">
+      <div className='underline underline-offset-8 decoration-2 decoration-orange-500 text-3xl px-16 font-bold text-white'>Our Work</div>
+      <LayoutGrid cards={cards} />
+    </div>
+  );
+}
+
+const cards = [
+  {
+    id: 1,
+    content: undefined,
+    className: "md:col-span-2",
+    thumbnail:
+      "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    content: undefined,
+    className: "col-span-1",
+    thumbnail:
+      "https://images.unsplash.com/photo-1464457312035-3d7d0e0c058e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    content: undefined,
+    className: "col-span-1",
+    thumbnail:
+      "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    content: undefined,
+    className: "md:col-span-2",
+    thumbnail:
+      "https://images.unsplash.com/photo-1475070929565-c985b496cb9f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+export default GridSection
