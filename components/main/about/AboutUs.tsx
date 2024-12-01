477828fbe89eeb8b7d32516c1b956289758dbae3
Atifsid <atifsiddiqui35@gmail.com>
2024-12-01 16:18:30 +0530

    add logo in about us

--- components/main/about/AboutUs.tsx
+++ components/main/about/AboutUs.tsx
@@ -1,5 +1,6 @@
 import styles from './AboutUs.module.css';
 import infoImg from '../../../public/static/images/info.png';
+import logo from '../../../public/static/images/logo.jpeg';
 import Image from 'next/image';
 
 export default function AboutUs() {
@@ -7,6 +8,9 @@ export default function AboutUs() {
         <section id='AboutUs' className={styles.aboutUs}>
             <div className={styles.container}>
                 <h1 className={styles.title}>About Us</h1>
+                <div className='flex justify-center my-6'>
+                    <Image src={logo} height={150} width={150} alt='logo' style={{ borderRadius: 12 }} />
+                </div>
                 <p className={styles.paragraph}>
                     We started our prestigious journey in <strong>2019</strong>, with the idea of making everybody reachable
                     for good quality media operations. So we started gathering some of the best quality equipment to enhance
